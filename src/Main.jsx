cfb3ff1401d56dcac7528effca9d73dded189310
Abdo5-Mahmoud <abdofwzy9@gmali.com>
2024-08-01 15:15:04 +0300

    edit api

--- src/Main.jsx
+++ src/Main.jsx
@@ -165,7 +165,7 @@ export function SelectedMovie({
     async function getMovieDetails() {
       setIsLoading(true);
       const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        `http://www.omdbapi.com/?apikey=b0833e1a&i=${selectedId}`
       );
       const data = await res.json();
       setMovie(data);
